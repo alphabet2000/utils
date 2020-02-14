b1111c426d0f9a278df9aee15e100fe7bb20e0ef
Lilong Li <lilong@innowatts.com>
2020-02-13 21:02:55 -0600

    num_threads

---
+++ utils/num_threads.py
@@ -0,0 +1,25 @@
+def set_num_threads(n):
+    try: 
+        import mkl
+        mkl.set_num_threads(n)
+    except:
+        pass
+    
+    n=str(n)
+    import os
+    os.environ['OPENBLAS_NUM_THREADS'] = n
+    os.environ['NUMEXPR_NUM_THREADS'] = n
+    os.environ['OMP_NUM_THREADS'] = n
+    os.environ['MKL_NUM_THREADS'] = n
+
+def reset_num_threads():
+    try:
+        import mkl
+        mkl.set_num_threads(0)
+    except:
+        pass
+    import os
+    del os.environ['OPENBLAS_NUM_THREADS']
+    del os.environ['NUMEXPR_NUM_THREADS']
+    del os.environ['OMP_NUM_THREADS']
+    del os.environ['MKL_NUM_THREADS']
